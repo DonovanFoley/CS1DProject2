ae5be7950bc6ec704ad90527852113a45d7bde56
kkamalin <158026165+kkamalin@users.noreply.github.com>
2024-05-13 14:04:59 -0700

    Update stadiummanager.h

--- stadiummanager.h
+++ stadiummanager.h
@@ -2,8 +2,11 @@
 #define STADIUMMANAGER_H
 
 #include "graph.h"
+#include "map.h"
 #include <QObject>
 #include <QMessageBox>
+#include <QWidget>
+#include <QInputDialog>
 #include <QFile>
 #include <QTextStream>
 #include <QStringList>
@@ -13,18 +16,26 @@ using namespace std;
 
 class StadiumManager: public QObject
 {
-     Q_OBJECT
+    Q_OBJECT
 public:
+    StadiumManager(Graph& graph, Map& teams);   //new
+
     void loadStadiums();
     void performDFS(QWidget *parentWidget);
     void performBFS(QWidget *parentWidget);
 
+
+    std::vector<int> convertTeamNamesToStadiumIndices(const std::vector<QString>& teamNames); //new
+    std::vector<QString> getUserInputTeamNames(QWidget* parentWidget); //new
+
 private:
-    Graph graph;
+    Graph& graph;
+    Map& teams;
+
     unordered_map<QString, int> StadiumToIndex;
     unordered_map<int, QString> indexToStadium;
+
+    
 };
 
 #endif // STADIUMMANAGER_H
-
-
